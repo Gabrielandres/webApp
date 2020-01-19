57fb654653187f8030a05c788b50eaeeeb077aef
Gabrielandres <san16047@byui.edu>
2020-01-18 20:47:52 -0700

    change added to displayTime.php file

--- web/displayTime.php
+++ web/displayTime.php
@@ -6,10 +6,11 @@
 <body background="background2.jpg">
 
 <?php
+
+date_default_timezone_set("America/Boise");
 echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("l") . "<br>";
 
-date_default_timezone_set("America/Boise");
 echo "The time is " . date("h:i:sa");
 ?>
 
