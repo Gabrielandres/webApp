9dc70ce36e93b7f6f97a2fe88c95cf4f3ecda098
Gabrielandres <san16047@byui.edu>
2020-02-17 19:33:04 -0700

    test#18

--- web/register.php
+++ web/register.php
@@ -10,16 +10,16 @@ $db = get_db();
 
 if(isset($_POST['register'])){
     
-   $user = !empty($_POST['username']) ? trim($_POST['username']) : null;
+   $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
    $pass = !empty($_POST['password']) ? trim($_POST['password']) : null;
 	echo "<p>$user $pass</p>";
 	
    
- $stmt = $db->prepare("SELECT COUNT(username) AS num FROM users WHERE username = :user");
+ $stmt = $db->prepare("SELECT COUNT(username) AS num FROM users WHERE username = :username");
 /*
  $stmt1 = $db->prepare("SELECT username, password FROM users");
 
-    $stmt->bindValue(':username', $user);
+    $stmt->bindValue(':username', $username);
     
     $stmt1->execute();
 	while ($row = $stmt1->fetch(PDO::FETCH_ASSOC))
@@ -40,10 +40,10 @@ if(isset($_POST['register'])){
 
 $passwordHash = password_hash($pass, PASSWORD_BCRYPT, array("cost" => 12));
 
-    $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (:user, :pass)");
+    $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
 
- $stmt->bindValue(':user', $username);
-    $stmt->bindValue(':pass', $passwordHash);
+ $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':password', $passwordHash);
 
 $result = $stmt->execute();
     
