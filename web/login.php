2ba78c70c93b74321db7efa23e33a1339768ecc0
Gabrielandres <san16047@byui.edu>
2020-02-18 02:02:18 -0700

    test#29

--- web/login.php
+++ web/login.php
@@ -11,12 +11,26 @@ if(isset($_POST['login'])){
 	
 	$username = !empty($_POST['username']) ? trim($_POST['username']) : null;
     $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+	
+$stmt = $db->prepare("SELECT username, password FROM users");
 
+    $stmt->bindValue(':username', $username);
+    
+    $stmt1->execute();
+	while ($row = $stmt1->fetch(PDO::FETCH_ASSOC))
+{
+	
+	$username = $row['username'];
+	$password = $row['password'];
+
+	echo "<p><strong>$username $password</strong></p>";
+}	
+/*
     $stmt = $db->prepare("SELECT id, username, password FROM users WHERE username = :username");
     $stmt->bindValue(':username', $username);
     $stmt->execute();
 	$user = $stmt->fetch(PDO::FETCH_ASSOC);
-/*	
+	
 	if($user === false){
      
         die('Incorrect username / password combination!');
