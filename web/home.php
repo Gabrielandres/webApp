7e53c17d833c527bab654560b3cb0a3dbef9f3ee
Gabrielandres <san16047@byui.edu>
2020-02-25 11:07:00 -0700

    test#62

--- web/home.php
+++ web/home.php
@@ -5,6 +5,22 @@
 		<link rel="stylesheet" type="text/css" href="proStyle.css" />
     </head>
     <body>
+	
+	<h1>What would you like to learn more about today?</h1>
+	<h2>A dropdown menu is provided so that you can select the material 
+	in our database relevant to your insterests!!</h2>
+	<div class="dropdown">
+       <button class="dropbtn">Material Available in our 
+	   database.</button>
+       <div class="dropdown-content">
+          <a href="#">Scriptures</a>
+          <a href="#">General Conferences Talks</a>
+          <a href="#">Ensign Articles</a>
+		  <a href="#">Hymns</a>
+		  <a href="#">Lds Manuals</a>
+        </div>
+     </div>
+	
        
     </body>
 </html>
